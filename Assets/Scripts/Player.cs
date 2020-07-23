fc2edcf81359600ce60e8b3b193de8f36d1a7c86
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2020-07-23 15:29:21 -0700

    Revert "Double Jump/Animations"
    This reverts commit 7790cf0320a91d9826fb6136c8e41392e7e35b36.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+<<<<<<< HEAD
     public float speed; //Allows the designer to set the player's speed in the inspector.
     public float jumpForce; //Allows the designer to set the player's jump height in the inspector.
     private float height = .5f; //Used for the raycast that checks for the ground.
@@ -13,11 +14,20 @@ public class Player : MonoBehaviour
     private SpriteRenderer sprite; //Holds the player's sprite renderer component.
     public LayerMask floorLayerMask; //Allows the designer to designate what counts as the floor for jumping.
     private Animator anim; //Holds the player's animator component.
+=======
+    public float speed;
+    public float jumpForce;
+    public float height = 1.1f;
+    public int maxMultiJump;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+>>>>>>> parent of 7790cf0... Double Jump/Animations
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+<<<<<<< HEAD
         anim = GetComponent<Animator>();
     }
 
@@ -28,12 +38,21 @@ public class Player : MonoBehaviour
             GameManager.instance.player = this.gameObject;
         }
         currentJumps = maxMultiJump;
+=======
+>>>>>>> parent of 7790cf0... Double Jump/Animations
     }
 
     private void Update()
     {
         Movement();
+<<<<<<< HEAD
         Jump();
+=======
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+>>>>>>> parent of 7790cf0... Double Jump/Animations
     }
 
     private void Movement()
@@ -43,6 +62,7 @@ public class Player : MonoBehaviour
 
         rb.velocity = new Vector2(xMovement, rb.velocity.y);
 
+<<<<<<< HEAD
         if (rb.velocity.x != 0)
         {
             anim.Play("Moving");
@@ -57,12 +77,18 @@ public class Player : MonoBehaviour
             //sprite.flipX = false;
         }
         else if (rb.velocity.x < 0)
+=======
+        if(rb.velocity.x > 0)
         {
-            //sprite.flipX = true;
+            sprite.flipX = false;
+        } else if (rb.velocity.x < 0 )
+>>>>>>> parent of 7790cf0... Double Jump/Animations
+        {
+            sprite.flipX = true;
         }
-
     }
 
+<<<<<<< HEAD
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Allows the player to grab collectables.
@@ -88,12 +114,17 @@ public class Player : MonoBehaviour
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             }
         }
+=======
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce);
+>>>>>>> parent of 7790cf0... Double Jump/Animations
     }
 
     //Uses a raycast to check for the floor. Used for double jumps and animations.
     bool isGrounded()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, (height / 2f) + 0.1f, floorLayerMask);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, (height / 2f) + 0.1f);
 
         return (hitInfo.collider != null);
     }
